de959d5d4bafd3296c1abb41e1c418dab71fd382
ZioS-ZkZ <thehao210502@gmail.com>
2022-03-24 23:35:40 +0700

    encryption ASE

--- ChuKySoRSA/fMain.cs
+++ ChuKySoRSA/fMain.cs
@@ -12,9 +12,6 @@ namespace ChuKySoRSA
         public fMain()
         {
             InitializeComponent();
-            //ẩn hai nút chọn đường dẫn (chờ set key rồi hiện sau ) 
-            
-            
         }
    
         //event mở file muốn ký và chọn thư mục
@@ -34,6 +31,8 @@ namespace ChuKySoRSA
             btKiemTra.Enabled = true;// hiện nút kiểm tra
         }
         //event tạo chữ ký
+        Aes myAes;
+        byte[] encrypted;
         private void btTaoChuKy_Click(object sender, EventArgs e)
         {
             // kiểm tra tồn tại path không
@@ -44,11 +43,18 @@ namespace ChuKySoRSA
             if (File.Exists(textDuongDanGui.Text))
             {
                 //mở file ra
+                
+                StreamReader fileReader = new StreamReader($"{textDuongDanGui.Text}");
+                string text = fileReader.ReadToEnd();
+                fileReader.Close();
+                fileReader.Dispose();
                 FileStream fsFileDauVao = new FileStream(textDuongDanGui.Text, FileMode.Open);
-
+                myAes = Aes.Create();
+                encrypted = EncryptStringToBytes_Aes(text, myAes.Key, myAes.IV);
                 SHAKE256 myShake256 = new SHAKE256(256);
                 byte[] byteMaHoa = myShake256.ComputeVariable(fsFileDauVao);
                 string FileVBKy = Convert.ToBase64String(byteMaHoa);
+                
                 Edwards448 myEd448 = new Edwards448();
                 byte[] sign = myEd448.Sign(byteMaHoa);
                 textHienThiPublicKey.Text = Convert.ToBase64String(myEd448.PublicKey);
@@ -72,9 +78,11 @@ namespace ChuKySoRSA
                 FileStream fsFileDauVao = new FileStream(textDuongDanNhan.Text, FileMode.Open);
                 SHAKE256 myShake256 = new SHAKE256(256);
                 byte[] byteGiaiMa = myShake256.ComputeVariable(fsFileDauVao);
+                string roundtrip = DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+                
                 Edwards448 giaiMa = new Edwards448();
                 var result = giaiMa.Verify(byteGiaiMa, Convert.FromBase64String(textHienThiPublicKey.Text), Convert.FromBase64String(textTepKyGui.Text));
-                // kiểm tra xem file giải mã bằng public key có trùng với văn bản đi kèm không
+
                 if (result)
                 {
                    MessageBox.Show("Tài liệu gửi đến không bị chỉnh sửa gì", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -87,16 +95,96 @@ namespace ChuKySoRSA
                 }
                 fsFileDauVao.Close();
                 fsFileDauVao.Dispose();
+                
             }
         }
-
-
         private void fMain_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (MessageBox.Show("Bạn muốn thoát khỏi ứng dụng ???", "THÔNG BÁO", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
                 e.Cancel = true;
         }
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
 
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
 
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+        
     }
 }
